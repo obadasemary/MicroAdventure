f720722fecaa09ce2330847076e15633056f3854
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-02-05 21:09:07 +0300

    Initial Commit

---
+++ MicroAdventure/MicroAdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroAdventureApp.swift
+//  MicroAdventure
+//
+//  Created by Abdelrahman Mohamed on 05.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MicroAdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MicroAdventureTests/MicroAdventureTests.swift
@@ -0,0 +1,17 @@
+//
+//  MicroAdventureTests.swift
+//  MicroAdventureTests
+//
+//  Created by Abdelrahman Mohamed on 05.02.2026.
+//
+
+import Testing
+@testable import MicroAdventure
+
+struct MicroAdventureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
